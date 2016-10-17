359fd86975c6f88975df4743324f57e0e71e279e
TexZK <texzk@email.it>
2016-10-18 00:20:26 +0200

    Changed test for WLF as IMF

--- oply/tests/test_imf.py
+++ oply/tests/test_imf.py
@@ -9,7 +9,9 @@ import oply.utils
 
 class TestIMF(TestCase):
 
+    def _render(self, imf_path, wav_path, rate=700):
         with open(imf_path, 'rb') as file:
+            imf = oply.utils.IMF(file, rate=rate)
         sequencer = oply.utils.Sequencer(imf.rate)
         imf.to_sequence(sequencer)
         opl3 = oply.opl3.Chip()
@@ -24,5 +26,7 @@ class TestIMF(TestCase):
                 wave_stream.writeframesraw(struct.pack('<hh', *sample))
 
     def test_convert_wolf3d_adlib_38(self):
+        self._render('adlib_38.wlf', 'adlib_38.wav')
 
     def test_convert_wolf3d_music_14(self):
+        self._render('music_14.wlf', 'music_14.wav')
